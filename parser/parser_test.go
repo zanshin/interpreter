946bba9ccb8f43e209373d62bd282823bf7e6a22
Mark Nichols <mark@zanshin.net>
2021-01-22 17:46:32 -0600

    added error handling

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -8,14 +8,17 @@ import (
 )
 
 func TestLetStatements(t *testing.T) {
-	input := `let x = 5;
-let y = 10;
-let foobar = *#*#*#;
+	input := `
+let x 5;
+let = 10;
+let 838383
 `
 	l := lexer.New(input)
 	p := New(l)
 
 	program := p.ParseProgram()
+	checkParseErrors(t, p)
+
 	if program == nil {
 		t.Fatalf("ParseProgram() returned nil")
 	}
@@ -63,4 +66,18 @@ func testLetStatement(t *testing.T, s ast.Statement, name string) bool {
 	}
 
 	return true
+}
+
+func checkParseErrors(t *testing.T, p *Parser) {
+	errors := p.Errors()
+	if len(errors) == 0 {
+		return
+	}
+
+	t.Errorf("parser has %d errors", len(errors))
+	for _, msg := range errors {
+		t.Errorf("parser error: %q", msg)
+	}
+
+	t.FailNow()
 }
