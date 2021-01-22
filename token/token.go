fc02eb162d77e94e9b7b2f0c6cea5d8e3985111f
Mark Nichols <mark@zanshin.net>
2021-01-22 13:58:38 -0600

    Added -, !, *, /, <, and >

--- token/token.go
+++ token/token.go
@@ -16,8 +16,15 @@ const (
 	INT   = "INT"   // 1234567890
 
 	// Operators
-	ASSIGN = "="
-	PLUS   = "+"
+	ASSIGN   = "="
+	PLUS     = "+"
+	MINUS    = "-"
+	BANG     = "!"
+	ASTERISK = "*"
+	SLASH    = "/"
+
+	LT = "<"
+	GT = ">"
 
 	// Delimiters
 	COMMA     = ","
