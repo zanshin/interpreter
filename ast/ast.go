2d07106570c7118b00880e7d451e8caadef81d4c
Mark Nichols <mark@zanshin.net>
2022-06-03 09:13:33 -0500

    Infix expressions now parsing

--- ast/ast.go
+++ ast/ast.go
@@ -128,12 +128,12 @@ func (il *IntegerLiteral) String() string       { return il.Token.Literal }
 
 // PREFIX Expression
 type PrefixExpression struct {
-	Token token.Token // the prefix token, e.g., !
+	Token    token.Token // the prefix token, e.g., !
 	Operator string
-	Right Expression
+	Right    Expression
 }
 
-func (pe *PrefixExpression) expressionNode() {}
+func (pe *PrefixExpression) expressionNode()      {}
 func (pe *PrefixExpression) TokenLiteral() string { return pe.Token.Literal }
 func (pe *PrefixExpression) String() string {
 	var out bytes.Buffer
@@ -141,6 +141,28 @@ func (pe *PrefixExpression) String() string {
 	out.WriteString("(")
 	out.WriteString(pe.Operator)
 	out.WriteString(pe.Right.String())
+	out.WriteString(")")
+
+	return out.String()
+}
+
+// INFIX Expression
+type InfixExpression struct {
+	Token    token.Token // the operator token, e.g., +
+	Left     Expression
+	Operator string
+	Right    Expression
+}
+
+func (ie *InfixExpression) expressionNode()      {}
+func (ie *InfixExpression) TokenLiteral() string { return ie.Token.Literal }
+func (ie *InfixExpression) String() string {
+	var out bytes.Buffer
+
+	out.WriteString("(")
+	out.WriteString(ie.Left.String())
+	out.WriteString(" " + ie.Operator + " ")
+	out.WriteString(ie.Right.String())
 	out.WriteString(")")
 
 	return out.String()
