e1e3ba6e0408cbc6c9ff6194e987a2e8812f6dbd
Mark Nichols <mark@zanshin.net>
2022-06-04 22:55:36 -0500

    Added parsing for Booleans

--- ast/ast.go
+++ ast/ast.go
@@ -167,3 +167,13 @@ func (ie *InfixExpression) String() string {
 
 	return out.String()
 }
+
+// BOOLEAN literals
+type Boolean struct {
+	Token token.Token
+	Value bool
+}
+
+func (b *Boolean) expressionNode()      {}
+func (b *Boolean) TokenLiteral() string { return b.Token.Literal }
+func (b *Boolean) String() string       { return b.Token.Literal }
